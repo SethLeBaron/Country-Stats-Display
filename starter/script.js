1706ea8a9d432becc4856e60734244165b9da663
SethLeBaron <sethrellhood@gmail.com>
2024-10-17 23:35:34 -0600

    added files

---
+++ starter/script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open(
+    'GET',
+    `https://countries-api-836d.onrender.com/countries/name/${country}`
+  );
+  request.send(request.responseText);
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    const html = `
+  <article class="country">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)}M people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].name
+            }</p>
+          </div>
+        </article>`;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+getCountryData('japan');
+getCountryData('usa');
+let times = 0;
+document.getElementById('submitButton').addEventListener('click', function () {
+  times += 1;
+  if (times > 10) {
+    alert('Limit of 12 entries, refresh to try different countries');
+    return;
+  }
+  const userInput = document.getElementById('userInput').value;
+  getCountryData(userInput.toLowerCase());
+});
